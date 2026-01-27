3666e2965789e0269df9a431aed3d7cc0951168e
Muhammad Shaikh Ramish <debator2006@gmail.com>
2026-01-28 00:20:31 +0500

    Add files via upload

---
+++ 22-01-2026/script.js
@@ -0,0 +1,382 @@
+// console.log("Hello world");
+
+// fullName = "Ramish";
+// console.log(fullName);
+
+// let name = "shaikh";
+// console.log(name);
+
+// const username = "ali";
+// console.log(typeof username)
+
+// {
+//     let aname = "alyan";
+//     console.log(aname);
+// }
+
+// const student = {
+//     name:"Ramish",
+//     age: 18,
+//     marks: 102,
+//     isPass: true,
+// };
+
+// student["age"] = 19;
+// student["age"] = student["age"] +1;
+// console.log(student);
+// console.log(student["isPass"]);
+// console.log(student.age);
+
+
+// let a = 5;
+// let b = 9;
+
+// console.log(a + b);
+// console.log("a + b = ",a + b);
+
+// let a = 5;
+// let b = 4;
+//   console.log(--a + --b);
+
+//   console.log(a + b);
+//   console.log(a++);
+//   console.log(a);
+//   console.log(++a);
+
+//   let a = 5;
+//   let b = 4;
+
+//     a  += 5;
+//   console.log("a =",a);
+
+// comparison operator
+// let a = 5;
+// let b = "5";
+// console.log( "a == b", a == b);
+// console.log(a == b);
+// console.log( a === b);
+
+    // logical operatotr
+// let a = 6;
+// let b = 3;
+//   let cond1 = a > b;
+//   let cond2 = a ===5;
+// console.log(cond1 && cond2);
+
+// let a = 6;
+// let b = 3;
+//   let cond1 = a > b;
+//   let cond2 = a ===5;
+// console.log(cond1 || cond2);
+
+//  conditional statements
+//   if statements
+
+// let age = 25;
+//  if (age > 18) {
+//     console.log("you can vote");
+//  } else {
+//     console.log("you cannot vote");
+//  }
+
+
+    // even odd
+
+// let num = 10;
+//  if ( num % 2 === 0 ) {
+//     console.log("even");
+//  } else {
+//     console.log("odd");
+//  }
+   
+    // else if statements
+//     let day = "monday";
+// if (day == "saturday") {
+//     console.log("Today is saturday");
+// } else if (day == "sunday") {
+//     console.log("Today is sunday");
+// } else if (day == "monday") {
+//     console.log("Today is monday");
+// } else if (day == "tuesday") {
+//     console.log("Today is tuesday");
+// } else if (day == "wednesday") {
+//     console.log("Today is wednesday");
+// } else if (day == "Thursday") {
+//     console.log("Today is Thursday");
+// } else if (day == "Friday") {
+//     console.log("Today is friday");
+// } else {
+//     console.log("No day found");
+// }
+
+
+    //    PRACTISE OF JAVA SCRIPT 
+
+    //Variables
+    
+    // console.log("hello world");
+
+    // fullName = "Ramish";
+    // console.log(fullName);
+
+    // let name = "tony stark";
+    // console.log(name);
+    // console.log(typeof(name))
+
+    // {
+    //     let name = "shaikh";
+    //     console.log(name);
+        
+    // }
+      
+    //   //Data types
+
+    // const students = {
+    //     name:"ramish",
+    //     age:18,
+    //     cgpa:1.9,
+    //     isPass:"true",
+    // };
+    // console.log(students);
+    // console.log(students["age"]);
+    // console.log(students.name);
+    
+    // students["age"] = students["age"] + 1;
+    //  console.log(students.age);
+    // students[name] = "ramishaa";
+    // console.log(students[name]);
+    
+    // //Operators
+
+    // //airthmetic opertors
+
+    // let a = 5;
+    // let b = 4;
+    // console.log(a+b);
+    // console.log("a + b = ", a + b);
+    
+    // //Uranary operators
+
+    // console.log(++a);
+    // console.log(a++);
+    // console.log(a);
+    // console.log(--b);
+    // console.log(b--);
+    // console.log(b);
+
+    // console.log( a++ + --b);
+    // console.log(a);
+    
+    // //Assignment operators
+
+    // let c = 5;
+    // let d = 4;
+    // c += 9;
+    // console.log(c);
+     
+    // c -= 2;
+    // console.log(c);
+
+    // c *= 2;
+    // console.log(c);
+
+    // c /= 2;
+    // console.log(c);
+
+    //comparison operators
+
+    // let a = 5;
+    // let b = 4;
+    // console.log( a == b);
+    // console.log( a != b);
+
+    //  let a = 5;
+    //  let b = "5";
+    //  console.log( a == b );
+    //  console.log( a === b);
+
+    //  let a = 5 ;
+    //  let b = 3;
+    //  console.log( a !== b );
+    //  console.log( ++a !== b );
+
+    // let a = 5;
+    // let b = 6;
+    // console.log( a > b);
+    // console.log( a < b);
+    // console.log( a >= b);
+    // console.log( a <= b);
+    
+    //logical opertors
+     
+    // a = 5;
+    // b = 5;
+    // let cond1 = a > b;
+    // let cond2 = a === b;
+
+    // console.log(cond1 && cond2);
+    // console.log(cond1 || cond2);
+
+    // console.log( !(a === b));
+
+    //Consitional Statement
+
+    // let num = 10;
+    // if (num %2 === 0 ) {
+    //     console.log("even")
+    // } else {
+    //     console.log("odd");
+        
+    // }
+    
+    // if , else, else if statements
+    // let day = "monday";
+    // if ( day == "saturday") {
+    //     console.log("Today is Saturday");
+    // } else if ( day == "sunday") {
+    //     console.log("Today is sunday");
+    // } else if ( day == "monday") {
+    //     console.log("Today is monday");
+    // }else if ( day == "tuesday") {
+    //     console.log("Today is tuesday");
+    // }else if ( day == "wednesday") {
+    //     console.log("Today is wednesday");
+    // }else if ( day == "thursday") {
+    //     console.log("Today is thursday");
+    // }else if ( day == "friday") {
+    //     console.log("Today is friday");
+    // } else {
+    //     console.log("No day found");
+    // }
+
+    //==========================LOOP===================================//
+
+    // for(let i = 1; i <= 5; i++) {
+    //     console.log("Ramish");
+    // }
+
+
+     //========================ARRAY====================================//
+
+    //  let array = ["ramish","shamil","azzam","ahmed","simala"];
+    //  console.log(array);
+    //  console.log(array.length);
+    //  console.log(array[0]);
+    //  console.log(array[4]);
+
+    // for(let i = 0; i < array.length; i++) {
+    //     console.log(array[i]);
+    // }
+     
+    //  for(let arr of array) {
+    //     console.log(arr);
+    //  }
+     
+    //  let cities = ["karachi","lahore","mirpur","hydrabad","kashmir"];
+    //  console.log(cities);
+     
+    //  for(let city of cities){
+    //     console.log(city);
+    //     console.log(city.toUpperCase());
+    //  }
+     
+     
+     //=================Array method=====================//
+
+    //  let item = ["apple","banana","mango","tomato","orange"];
+    //  console.log(item);
+    //  console.log(item.toString());
+
+    //  item.pop();
+    //  console.log(item);
+    //  let deleteditem = item.pop()
+    //  console.log(deleteditem);
+     
+
+    //  item.push("aalo");
+    //  console.log(item);
+     
+    //  item.unshift("patlo");
+    //  console.log(item);
+
+    //  item.shift();
+    //  console.log(item);
+
+    //  let val = item.shift();
+    //  console.log(val);
+     
+     
+     //===============================concat============================//
+
+    //  let marvelheros = ["azzam","shamil","ahmed","muhammad","ramish","simala"];
+    //  let dcheros = ["sufiyan","malik"];
+
+    //  let heros = marvelheros.concat(dcheros);
+    //  console.log(heros);
+
+    // let brothers = ["malik","sufyan","hannan","bilal","hassan","ahmed"];
+    // console.log(brothers);
+    // console.log(brothers.slice(1,3));
+    
+    // let arr = [1,2,3,4,5,6,7];
+    // console.log(arr);
+    // console.log(arr.splice(2,2,101));
+    // console.log(arr);
+
+    // arr.splice(2,0,11);
+    // console.log(arr);
+    
+    // arr.splice(3,1);
+    // console.log(arr);
+    
+    //  arr.splice(2,0,11)
+    // console.log(arr);
+    
+    // arr.splice(3,1);
+
+    // arr.splice(3,1,90);
+    // console.log(arr);
+    
+    
+
+    
+    
+    
+    
+    
+
+
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
