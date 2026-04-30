f9cbe9a39b2b57bcc93cfd64709886a70386f3a5
Muhammad Shaikh Ramish <debator2006@gmail.com>
2026-05-01 03:16:11 +0500

    Add files via upload

---
+++ Functions and methods/app.js
@@ -0,0 +1,31 @@
+function greet() {
+    console.log("hello world");
+}
+greet();
+
+function sum(a,b){
+    console.log(a + b);
+}
+
+sum(4,5);
+sum(12,5);
+
+
+function sum (x,y) {
+    s = x+y;
+    return s;
+}
+
+let val = sum(3,4);
+console.log(val);
+
+
+
+//Arrow function
+
+const arrowSum = (a,b) => {
+    console.log(a+b);
+}
+arrowSum(3,4);
+
+
